941cc0f61e2e0d97a61bab87bc2d4d8fd47ad465
yukio-sato <112220117+yukio-sato@users.noreply.github.com>
2024-08-08 18:37:11 -0300

    [08/08/2024] Updated conexao.php

--- conexao.php
+++ conexao.php
@@ -4,9 +4,11 @@ $username = "forumetec"; // Nome do Usuario do MYSQLI
 $password = "f0rum3t3cab!"; // Senha (Normalmente: f0rum3t3cab!)
 $dbname = "db_cadastro"; // Nome do Database
 
+/*
 $servername = "localhost"; // Link do Azure
 $username = "root"; // Nome do Usuario do MYSQLI
 $password = "root"; // Senha (Normalmente: f0rum3t3cab!)
+*/
 // Create connection
 $conn2 = new mysqli($servername, $username, $password);
 
